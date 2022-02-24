8f6e7e111829d9147ec270950d9f6de7b075138c
Rottie <kingtang2180@gmail.com>
2022-02-25 01:52:01 +0800

    ds

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 
 const cors = require("cors");
 
-const path = require("path");
 //-----------------------------------------------------
 const passport = require("passport");
 //Atuh
@@ -44,14 +43,6 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  // All other GET requests not handled before will return our React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 const PORT = process.env.PORT || 5000;
 
 // setting port 3000
